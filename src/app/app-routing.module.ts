ea450358e9f6207842b16fdec085f0905a426f35
Bondoc Marcian <marcy_whitewolf2010@yahoo.com>
2020-07-15 11:07:36 +0300

    hil-branch-2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
+import { HttpClientModule } from '@angular/common/http'
 
 const routes: Routes = [
   {
@@ -10,7 +10,10 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    HttpClientModule,
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
