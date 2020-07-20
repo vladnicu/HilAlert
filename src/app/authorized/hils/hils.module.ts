f925bdb2308cb2610d6efda3da4c2c7a971af3a3
Bondoc Marcian <marcy_whitewolf2010@yahoo.com>
2020-07-20 22:07:15 +0300

    added cards on main route

--- src/app/authorized/hils/hils.module.ts
+++ src/app/authorized/hils/hils.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { HilsRoutingModule } from './hils-routing.module';
 import { IndexComponent } from './components/index/index.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 @NgModule({
   declarations: [IndexComponent],
   imports: [
     CommonModule,
-    HilsRoutingModule
+    HilsRoutingModule,
+    SharedModule
   ]
 })
 export class HilsModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,13 +4,16 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { FormsModule } from '@angular/forms'
 import { MatTableModule } from '@angular/material/table'  
-
+import { MatGridListModule } from '@angular/material/grid-list'
+import { MatCardModule } from '@angular/material/card'
 const matModules = [
   MatDialogModule,
   MatCheckboxModule,
   FormsModule,
   MatTableModule,
-  MatDialogModule
+  MatDialogModule,
+  MatGridListModule,
+  MatCardModule
 ];
 
 @NgModule({
