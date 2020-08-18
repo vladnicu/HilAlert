7f98a1f3bbdd862ad76f6df44d1a1ee3b993055b
Bondoc Marcian <marcy_whitewolf2010@yahoo.com>
2020-08-18 16:27:44 +0300

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http'
-
+import { SharedModule } from './shared/shared.module';
 const routes: Routes = [
   {
     path: '',
@@ -13,6 +13,7 @@ const routes: Routes = [
   imports: [
     RouterModule.forRoot(routes),
     HttpClientModule,
+    SharedModule
   ],
   exports: [RouterModule]
 })
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,7 +7,7 @@ import { MatTableModule } from '@angular/material/table'  ;
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-
+import { MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 const matModules = [
   MatDialogModule,
   MatCheckboxModule,
@@ -16,7 +16,8 @@ const matModules = [
   MatDialogModule,
   MatGridListModule,
   MatCardModule,
-  MatButtonModule
+  MatButtonModule,
+  MatProgressSpinnerModule
 ];
 
 @NgModule({
