9c439eee3da6d0717804f28ac7608832ce672e90
Vlad Nicu <Vlad.Nicu@trw.com>
2020-08-28 11:56:01 +0300

    display data only if the hil has first entry

--- src/app/shared/services/hil.service.ts
+++ src/app/shared/services/hil.service.ts
@@ -1,32 +1,26 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
+export interface Hil {
+  labcarname: string;
+  firsthilentry: HilEntry;
+}
 
-export interface HilEntry{
+export interface HilEntry {
   machinename: string;
   osversion: string;
   projectname: string;
   date: string;
 }
 
-export interface Hil{
-  
-  labcarname: string;
-  firsthilentry : HilEntry;
-}
-
-
-
 @Injectable({
   providedIn: 'root',
 })
 export class HilService {
-  
   constructor(private http: HttpClient) {}
-  //private url : 'http://hilalertbackend.test/api/hils';
   getHils(): Observable<Hil[]> {
-    return this.http.get<Hil[]>(environment.apiUrl+ '/hils');
+    return this.http.get<Hil[]>(environment.apiUrl + '/hils');
   }
 }
