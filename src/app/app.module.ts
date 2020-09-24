56582078f9db06e6d01a4ed38a81dc6959d36622
Vlad Nicu <Vlad.Nicu@trw.com>
2020-09-24 11:19:36 +0300

    removed unused logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, APP_INITIALIZER } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
-import { PwaService } from 'src/app/shared/services/pwa.service';
-import { MatBottomSheet } from '@angular/material/bottom-sheet';
 import { ToastrModule } from 'ngx-toastr';
 import { FormsModule } from '@angular/forms';
 
-const initializer = (pwaService: PwaService) => () => pwaService.initPwaPrompt();
- 
 @NgModule({
   declarations: [
     AppComponent
@@ -26,9 +22,7 @@ const initializer = (pwaService: PwaService) => () => pwaService.initPwaPrompt()
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
     ToastrModule.forRoot()
   ],
-  providers: [
-    {provide: MatBottomSheet, useValue: {} },
-    {provide: APP_INITIALIZER, useFactory: initializer, deps: [PwaService], multi: true},],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/authorized/hils/hils.module.ts
+++ src/app/authorized/hils/hils.module.ts
@@ -4,7 +4,6 @@ import { CommonModule } from '@angular/common';
 import { HilsRoutingModule } from './hils-routing.module';
 import { IndexComponent } from './components/index/index.component';
 import { SharedModule } from 'src/app/shared/shared.module';
-import { PromptComponent } from './components/prompt-component/prompt-component.component';
 import { LogindialogComponent } from './components/logindialog/logindialog.component';
 
 
